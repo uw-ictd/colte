8e3754e2dc9725a43bd300ebc78a3cea2d2c6231
Spencer Sevilla <sevilla@cs.washington.edu>
2018-02-17 17:27:22 -0800

    adding an ansible preconfig script that should work but not yet tested

--- install_rocketchat.sh
+++ install_rocketchat.sh
@@ -1,4 +1,6 @@
 sudo apt-get install docker
 sudo apt install docker.io
+
+
 sudo docker run --name db -d mongo:3.0 --smallfiles
 sudo docker run --name rocketchat -p 8080:3000 --env ROOT_URL=http://localhost --link db -d rocket.chat
