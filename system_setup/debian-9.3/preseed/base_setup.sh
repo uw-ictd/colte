319b5f13bf6c6014a90d189082ee81844085e3a6
Spencer Sevilla <sevilla@cs.washington.edu>
2018-02-28 15:22:47 -0800

    vagrantfile for debian now added
    Former-commit-id: 1658a80a05560dfbcd560a594bee4d06ee146ff4

--- system_setup/debian-9.3/preseed/base_setup.sh
+++ system_setup/debian-9.3/preseed/base_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo apt-get -y install git
-git clone https://github.com/uw-ictd/colte.git /home/colte/colte
-/home/colte/colte/system_setup/debian-9.3/setup-debian-9.3.sh
+git clone https://github.com/uw-ictd/colte.git /home/$USER/colte
+/home/$USER/colte/system_setup/debian-9.3/setup-debian-9.3.sh
