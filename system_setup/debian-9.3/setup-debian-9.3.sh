0f8c211652dae3845be31225a6edd74dd61500c0
Spencer Sevilla <sevilla@cs.washington.edu>
2018-02-28 14:57:15 -0800

    big restructuring of usernmae mods in ansible scripts, should work fine, lets see

--- system_setup/debian-9.3/setup-debian-9.3.sh
+++ system_setup/debian-9.3/setup-debian-9.3.sh
@@ -9,7 +9,7 @@
 # The EPC will be 10.0.101.3
 
 # Step 1: Install prereqs (ansible and python)
-sudo cp /home/enbrain/colte/system_setup/debian-9.3/ansible_sources.list /etc/apt/sources.list
+sudo cp /home/$USER/colte/system_setup/debian-9.3/sources/ansible_sources.list /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install -y --allow-unauthenticated ansible python2.7
 
@@ -18,10 +18,10 @@ sudo apt-get install -y --allow-unauthenticated ansible python2.7
 #sudo apt-get update
 
 # Step 2: Run Ansible script to do all the rest of the setup
-sudo cp /home/enbrain/colte/system_setup/debian-9.3/sources.list /etc/apt/sources.list
+sudo cp /home/$USER/colte/system_setup/debian-9.3/sources/sources.list /etc/apt/sources.list
 sudo apt-get update
 
 sudo apt-get -y install vim
-ansible-playbook -v -i "localhost," -c local /home/enbrain/colte/system_setup/debian-9.3/ansible/main_playbook.yml 
+ansible-playbook -v -i "localhost," -c local /home/$USER/colte/system_setup/debian-9.3/ansible/main_playbook.yml 
 
 # Step 3: Any final-final configs?!? Setting IP addresses in config files, etc?!?
