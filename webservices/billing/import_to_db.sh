44e5a475d0007ef3adbc6c055e9c568acf7cef10
Spencer Sevilla <sevilla@cs.washington.edu>
2018-03-19 15:44:42 -0700

    changing oai_db to colte
    Former-commit-id: f54d2ca0f87dbf33d6e569a3e78329b79fb3bc95

--- webservices/billing/import_to_db.sh
+++ webservices/billing/import_to_db.sh
@@ -5,6 +5,6 @@
 
 source /usr/local/etc/colte/coltenv
 
-curl --cookie "user=admin; password=password" http://localhost:3000/lua/colte_log_bandwidth.lua > $COLTE_DIR/webservices/billing/tmp_dump.txt
+curl --cookie "user=admin; password=password" http://localhost/phpmyadmin/lua/colte_log_bandwidth.lua > $COLTE_DIR/webservices/billing/tmp_dump.txt
 
 python $COLTE_DIR/webservices/billing/import_to_db.py
