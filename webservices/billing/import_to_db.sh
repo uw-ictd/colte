f9b5216b404698d38ebc998e158e1d80a57a7fab
Spencer Sevilla <sevilla@cs.washington.edu>
2018-03-19 15:39:02 -0700

    more script improvements
    Former-commit-id: bf7cebd3a426afe6b1d5ea082f8e6c42cd9db306

--- webservices/billing/import_to_db.sh
+++ webservices/billing/import_to_db.sh
@@ -3,6 +3,8 @@
 # SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # COLTE_DIR="$SCRIPTDIR/../.."
 
+source /usr/local/etc/colte/coltenv
+
 curl --cookie "user=admin; password=password" http://localhost:3000/lua/colte_log_bandwidth.lua > $COLTE_DIR/webservices/billing/tmp_dump.txt
 
 python $COLTE_DIR/webservices/billing/import_to_db.py
