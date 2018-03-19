1672ece1835a023258079701007a309228b832de
Spencer Sevilla <sevilla@cs.washington.edu>
2018-03-19 15:44:42 -0700

    changing oai_db to colte
    Former-commit-id: f54d2ca0f87dbf33d6e569a3e78329b79fb3bc95

--- epc/spencer_configs/sample_db.sql
+++ epc/spencer_configs/sample_db.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.18, for Linux (x86_64)
 --
--- Host: localhost    Database: oai_db
+-- Host: localhost    Database: colte
 -- ------------------------------------------------------
 -- Server version	5.7.18-0ubuntu0.17.04.1
 
