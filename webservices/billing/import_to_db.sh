37adbff2890c9b8dc8089dc432a1ce8f435b0a65
Spencer Sevilla <sevilla@cs.washington.edu>
2018-03-19 19:41:50 -0700

    can we get a cronjob to work reliably
    Former-commit-id: 5524866f3647508fda77f31fe3f20d8a526ce08a

--- webservices/billing/import_to_db.sh
+++ webservices/billing/import_to_db.sh
@@ -5,6 +5,7 @@
 
 source /usr/local/etc/colte/coltenv
 
-curl --cookie "user=admin; password=password" http://localhost/phpmyadmin/lua/colte_log_bandwidth.lua > $COLTE_DIR/webservices/billing/tmp_dump.txt
+echo "RAN CRONSCRIPT" >> $COLTE_DIR/webservices/billing/cronscript_log.txt
 
+# curl --cookie "user=$COLTE_PMA_USERNAME; password=$COLTE_PMA_PASSWORD" http://localhost/phpmyadmin/lua/colte_log_bandwidth.lua > $COLTE_DIR/webservices/billing/tmp_dump.txt
 python $COLTE_DIR/webservices/billing/import_to_db.py
