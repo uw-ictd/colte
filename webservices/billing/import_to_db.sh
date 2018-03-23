9314d6f78263a56b6d93d3bbc7614b99c074aa70
Spencer Sevilla <sevilla@cs.washington.edu>
2018-03-23 13:57:03 -0700

    this should fully setup and start ntopng
    Former-commit-id: 1da9d7fa516b4e0634f9afc07f83e81df37492c6

--- webservices/billing/import_to_db.sh
+++ webservices/billing/import_to_db.sh
@@ -2,5 +2,6 @@
 
 source /usr/local/etc/colte/coltenv
 
-# curl --cookie "user=$COLTE_PMA_USERNAME; password=$COLTE_PMA_PASSWORD" http://localhost/phpmyadmin/lua/colte_log_bandwidth.lua > $COLTE_DIR/webservices/billing/tmp_dump.txt
+curl --cookie "user=$COLTE_NT_USERNAME; password=$COLTE_NT_PASS" http://localhost:3002/lua/colte_log_bandwidth.lua > $COLTE_DIR/webservices/billing/tmp_dump.txt
+
 python $COLTE_DIR/webservices/billing/import_to_db.py
