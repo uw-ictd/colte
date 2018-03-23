9e79cdb88eabfd8f90e12c2c4830616a9c6ba256
vagrant <vagrant@debian-9.3-amd64>
2018-03-23 23:14:12 +0000

    restructuring again
    Former-commit-id: 3c8c3b5b30df1e6405bfdb5c4c84e344d0262015

--- system_setup/debian-9.3/setup.sh
+++ system_setup/debian-9.3/setup.sh
@@ -21,23 +21,5 @@ sudo apt-get update
 
 sudo apt-get -y install vim curl
 
-if [ $COLTE_EPC == 1 ]; then
-	ansible-playbook -K -v -i "localhost," -c local $COLTE_DIR/epc/install_scripts/debian-9.3.yml
-fi
-
-if [ $COLTE_ENBRAINS == 1 ]; then
-	ansible-playbook -K -v -i "localhost," -c local $COLTE_DIR/enbrains/install_scripts/debian-9.3.yml
-fi
-
-if [ $COLTE_EMERGENCY_WEBSERVICES == 1 ]; then
-	ansible-playbook -K -v -i "localhost," -c local $COLTE_DIR/emergency_webservices/install_scripts/debian-9.3.yml
-fi
-
-if [ $COLTE_NODE == 1 ]; then
-	ansible-playbook -K -v -i "localhost," -c local $COLTE_DIR/webservices/node/install_scripts/debian-9.3.yml
-fi
-
-if [ $COLTE_BILLING == 1 ]; then
-	ansible-playbook -K -v -i "localhost," -c local $COLTE_DIR/webservices/billing/install_scripts/debian-9.3.yml
-fi
+$COLTE_DIR/system_setup/debian-9.3/ansible.sh
 
