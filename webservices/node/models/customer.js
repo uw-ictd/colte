974dbb1ff8c86a50cd3a1e0d2d7f002257b2bd41
Conner Hansen <hansenconners@gmail.com>
2018-04-09 15:21:40 -0700

    db update function first draft
    Former-commit-id: b0a7a5b593296d8864737ffcddc6db76536bfc4b

--- webservices/node/models/customer.js
+++ webservices/node/models/customer.js
@@ -11,6 +11,32 @@ var customer = {
   },
   create() {
 
+  },
+  change_balance(ip, delta) {
+    // IP should be switched with IMSI
+    async function executor(resolve, reject) {
+      var result = {
+        balance: -1.0,
+        success: false
+      };
+      await knex.select('balance').where('ip', ip).from('customers').then((data) => {
+        if (data.length != 1) return;
+        result.balance = data[0].balance;
+      });
+      // handle no result found better, exception?
+      if (result.balance < 0.0) {
+        resolve(result);
+        return;
+      }
+      if (result.balance + delta >= 0.0) {
+        // should ensure database update success before setting return values?
+        result.balance += delta;
+        result.success = true;
+        await knex.update({ balance: result.balance }).where('ip', ip).from('customers');
+      }
+      resolve(result);
+    }
+    return new Promise(executor);
   }
 }
 
