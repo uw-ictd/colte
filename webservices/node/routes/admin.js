000f4606d570f22bc113b9ed9521e02efbcb06de
pathiratk <pathirat.kos@gmail.com>
2018-04-12 17:43:07 -0700

    refresh page after update balance request
    Former-commit-id: 0dcd6ed30449a14fd870fafc29d11ae2871f922e

--- webservices/node/routes/admin.js
+++ webservices/node/routes/admin.js
@@ -12,11 +12,16 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res) {
+  var ip = "192.168.151.5";
+  var msisdn = req.body.msisdn;
   var newBalance = req.body.newBalance;
-  console.log(newBalance);
+  var delta 
   // call the Model and update the balance
-
+  customer.change_balance(ip, newBalance).then((data) => {
+    console.log(data);
+  })
   // res.status(404);
+  res.redirect(req.originalUrl);
   res.end();
 })
 
