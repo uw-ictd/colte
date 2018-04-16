bef83a6fc78b87c35c19b96c4804d300aad0e8d8
Conner Hansen <hansenconners@gmail.com>
2018-04-16 11:46:13 -0700

    balance transfer function comments
    Former-commit-id: 274737c469baacc4b3cf91fa4ce0218a5581a85c

--- webservices/node/models/customer.js
+++ webservices/node/models/customer.js
@@ -24,10 +24,15 @@ var customer = {
       return knex.update({balance: newBalance}).where('msisdn', msisdn).from('customers');
     })
   },
+  // Updates balance of the customer with given IMSI by delta (positive or negative)
+  // Will fail if balance would go negative
+  // return promise for a result object with:
+  // success of operation, balance after operation, and a failure message 
   update_balance(imsi, delta) {
     //TODO:
     // should reject (not resolve) on failure?
     // should ensure database update success before setting return values
+
     async function executor(resolve, reject) {
       var result = {
         success: false,
@@ -42,6 +47,7 @@ var customer = {
         }
         result.balance = Number(data[0]["balance"]);
       });
+      // validate new balance
       var nbalance = result.balance + Number(delta);
       if (nbalance >= 0.0) {
         result.balance = nbalance;
@@ -52,6 +58,10 @@ var customer = {
     }
     return new Promise(executor);
   },
+  // moves "amount" from the customer with sender_ip to the customer with receiver_msisdn
+  // amount must be positive non-negative
+  // returns promise with no data
+  // currently logs all actions to the console
   transfer_balance(sender_ip, receiver_msisdn, amount) {
     // TODO:
     // REFUND SENDER IF RECIEVER BALANCE FAILS TO UPDATE
@@ -60,8 +70,9 @@ var customer = {
     // real failure behavior (e.g. negative transfer)
     // log with date & time
     // log to actual file
-    // shouldn't chained promises return promises for promises? ask someone why this works
+
     amount = Number(amount);
+    // translate ip and msisdn to imsi's
     function fetch_imsis() {
       return knex.select('imsi').where('ip', sender_ip).from('customers').then((sender_match) => {
         return knex.select('imsi').where('msisdn', receiver_msisdn).from('customers')
@@ -69,14 +80,17 @@ var customer = {
       });
     }
   
+    // transfer using imsi's and update_balance
     function transfer(imsi_matches) {
       var logstr = "Transfer ";
+      // verify one match for sender ip
       if (imsi_matches[0].length != 1) {
         logstr += "failure: ip " + sender_ip + " matched " + imsi_matches[0].length + " database entries.";
         console.log(logstr);
         return;
       }
       var sender_imsi = imsi_matches[0][0]["imsi"];
+      // verify one match for receiver msisdn
       if (imsi_matches[1].length != 1) {
         logstr += "failure: msisdn " + receiver_msisdn + " matched " + imsi_matches[1].length + " database entries.";
         console.log(logstr);
@@ -89,20 +103,21 @@ var customer = {
         console.log(logstr);
         return;
       }
+      // update balances sequentially, sender then receiver
       customer.update_balance(sender_imsi, -amount).then((result0) => {
         if (!result0.success) {
-          logstr += "failure: update sender balance failed with result: " + result0;
+          logstr += "failure: update sender balance failed with result: " + JSON.stringify(result0);
           console.log(logstr);
           return;
         }
-        console.log("result0: " + result0);
+        console.log("result0: " + JSON.stringify(result0));
         customer.update_balance(receiver_imsi, amount).then((result1) => {
           if (!result1.success) {
-            logstr += "failure: update receiver balance failed with result: " + result1;
+            logstr += "failure: update receiver balance failed with result: " + JSON.stringify(result1);
             console.log(logstr);
             return;
           }
-          console.log("result1: " + result1);
+          console.log("result1: " + JSON.stringify(result1));
           logstr += "success: imsi " + sender_imsi + " sent imsi " + receiver_imsi + " " + amount + ".";
           console.log(logstr);
         });
