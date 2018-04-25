03bb15195824e4546f255560682b4bf74beae347
CoLTE Community LTE <colte@colte.colte>
2018-04-25 21:59:09 +0000

    epc now works great.

---
+++ clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo rm -rf /var/log/colte
+sudo rm -rf /usr/local/etc/oai
+sudo rm -rf /usr/local/etc/colte
+rm -rf packages/asn1c
+rm -rf packages/freediameter
+rm -rf packages/libgtpnl
+rm -rf packages/liblfds
