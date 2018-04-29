1231473d6f6481bc909b5fd6acc0dc8ee2bf2257
Spencer Sevilla <sevilla@cs.washington.edu>
2018-04-29 10:58:00 -0700

    minor fixes
    Former-commit-id: 1c2278e115d40b7af27ce5b655a952ed91726ea1

--- epc/src/nas/emm/sap/emm_send.h
+++ epc/src/nas/emm/sap/emm_send.h
@@ -84,7 +84,7 @@ Description Defines functions executed at the EMMAS Service Access
 int emm_send_status(const emm_as_status_t *, emm_status_msg *);
 
 int emm_send_detach_accept(const emm_as_data_t *, detach_accept_msg *);
-int emm_send_detach_request(const emm_as_data_t *, detach_accept_msg *);
+int emm_send_detach_request(const emm_as_data_t *, detach_request_msg *);
 
 int emm_send_attach_accept(const emm_as_establish_t *, attach_accept_msg *);
 int emm_send_attach_accept_dl_nas (const emm_as_data_t * msg, attach_accept_msg *);
