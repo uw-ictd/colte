c42586208822c9fcc9ef7b0288dd6a6f7a38e151
Spencer Sevilla <sevilla@cs.washington.edu>
2018-05-02 15:59:54 -0700

    drafting all database code seems to work well
    Former-commit-id: 3f8737860a5995d31cd901d091a9b0628223caa5

--- epc/spencer_configs/bokondini.sql
+++ epc/spencer_configs/bokondini.sql
@@ -98,7 +98,38 @@ CREATE TABLE `users` (
   KEY `fk_users_mmeidentity_idx1` (`mmeidentity_idmmeidentity`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `all_users`;
+CREATE TABLE `all_users` (
+  `imsi` varchar(15) NOT NULL COMMENT 'IMSI is the main reference key.',
+  `msisdn` varchar(46) DEFAULT NULL COMMENT 'The basic MSISDN of the UE (Presence of MSISDN is optional).',
+  `imei` varchar(15) DEFAULT NULL COMMENT 'International Mobile Equipment Identity',
+  `imei_sv` varchar(2) DEFAULT NULL COMMENT 'International Mobile Equipment Identity Software Version Number',
+  `ms_ps_status` enum('PURGED','NOT_PURGED') DEFAULT 'PURGED' COMMENT 'Indicates that ESM and EMM status are purged from MME',
+  `rau_tau_timer` int(10) unsigned DEFAULT '120',
+  `ue_ambr_ul` bigint(20) unsigned DEFAULT '50000000' COMMENT 'The Maximum Aggregated uplink MBRs to be shared across all Non-GBR bearers according to the subscription of the user.',
+  `ue_ambr_dl` bigint(20) unsigned DEFAULT '100000000' COMMENT 'The Maximum Aggregated downlink MBRs to be shared across all Non-GBR bearers according to the subscription of the user.',
+  `access_restriction` int(10) unsigned DEFAULT '60' COMMENT 'Indicates the access restriction subscription information. 3GPP TS.29272 #7.3.31',
+  `mme_cap` int(10) unsigned zerofill DEFAULT NULL COMMENT 'Indicates the capabilities of the MME with respect to core functionality e.g. regional access restrictions.',
+  `mmeidentity_idmmeidentity` int(11) NOT NULL DEFAULT '0',
+  `key` varbinary(16) NOT NULL DEFAULT '0' COMMENT 'UE security key',
+  `RFSP-Index` smallint(5) unsigned NOT NULL DEFAULT '1' COMMENT 'An index to specific RRM configuration in the E-UTRAN. Possible values from 1 to 256',
+  `urrp_mme` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'UE Reachability Request Parameter indicating that UE activity notification from MME has been requested by the HSS.',
+  `sqn` bigint(20) unsigned zerofill NOT NULL,
+ `rand` varbinary(16) NOT NULL,
+  `OPc` varbinary(16) DEFAULT NULL COMMENT 'Can be computed by HSS',
+  PRIMARY KEY (`imsi`,`mmeidentity_idmmeidentity`),
+  KEY `fk_users_mmeidentity_idx1` (`mmeidentity_idmmeidentity`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
 INSERT INTO `users` VALUES
+('910540000000995','628650000995',NULL,NULL,'PURGED',120,50000000,100000000,47,0,1,0x85def29fe66f28fa7081857ab98eb77e,1,0,351,0x0,0x826f0df3f2fc5d969e1676e843e61739),
+('910540000000996','628650000996',NULL,NULL,'PURGED',120,50000000,100000000,47,0,1,0x6cfa57b83812a4debca0af546253c009,1,0,351,0x0,0xacca1efd2b9611496713ab1381779f35),
+('910540000000997','628650000997',NULL,NULL,'PURGED',120,50000000,100000000,47,0,1,0xdd4f8e34bf666ebb4747404b36a2ca45,1,0,351,0x0,0xb142c2a4b45e16940d1239e531b47dfd),
+('910540000000998','628650000998',NULL,NULL,'PURGED',120,50000000,100000000,47,0,1,0x5b19bcd10a34fd15173b5432fa5d66e2,1,0,351,0x0,0x1e71bac1bbacf435634a9e7e290b3260),
+('910540000000999','628650000999',NULL,NULL,'PURGED',120,50000000,100000000,47,0,1,0x4a0978fcc07b522e7003e1a3695aab0e,1,0,351,0x0,0x992c513693239284fe779a5a177037a0)
+;
+
+INSERT INTO `all_users` VALUES
 ('910540000000995','628650000995',NULL,NULL,'PURGED',120,50000000,100000000,47,0,1,0x85def29fe66f28fa7081857ab98eb77e,1,0,351,0x0,0x826f0df3f2fc5d969e1676e843e61739),
 ('910540000000996','628650000996',NULL,NULL,'PURGED',120,50000000,100000000,47,0,1,0x6cfa57b83812a4debca0af546253c009,1,0,351,0x0,0xacca1efd2b9611496713ab1381779f35),
 ('910540000000997','628650000997',NULL,NULL,'PURGED',120,50000000,100000000,47,0,1,0xdd4f8e34bf666ebb4747404b36a2ca45,1,0,351,0x0,0xb142c2a4b45e16940d1239e531b47dfd),
