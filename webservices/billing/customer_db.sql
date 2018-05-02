552c59381e0c679f42e16a2d3ea952c84ba473ec
Spencer Sevilla <sevilla@cs.washington.edu>
2018-05-02 15:59:54 -0700

    drafting all database code seems to work well
    Former-commit-id: 3f8737860a5995d31cd901d091a9b0628223caa5

--- webservices/billing/customer_db.sql
+++ webservices/billing/customer_db.sql
@@ -2,13 +2,13 @@
 DROP TABLE IF EXISTS `customers`;
 CREATE TABLE `customers` (
   `idcustomers` int(11) NOT NULL AUTO_INCREMENT,
-  `ip` varchar(16) NOT NULL,
+  `imsi` varchar(16) NOT NULL,
   `raw_down` int(10) unsigned DEFAULT '0',
   `raw_up` int(10) unsigned DEFAULT '0',
   `balance` int(10) unsigned DEFAULT '0' COMMENT 'in USD for now',
-  `imsi` varchar(16) DEFAULT 'NotUsed',
+  `enabled` tinyint(1) DEFAULT '0',
   `msisdn` varchar(16) DEFAULT 'NotUsed',
   PRIMARY KEY (`idcustomers`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-INSERT INTO `customers` VALUES (1, '10.0.0.42', 0, 0, 500, 'NotUsed', 'NotUsed');
+INSERT INTO `customers` VALUES (1, '910540000000999', 0, 0, 500, 1, 'NotUsed');
