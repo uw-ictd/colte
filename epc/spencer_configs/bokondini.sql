32e625e9cf0a5aeb5e8041605e8fb862b20452b8
CoLTE Community LTE <colte@colte-epc.cs.washington.edu>
2018-05-07 20:40:12 +0000

    minor fixes but now all db code works
    Former-commit-id: 3be85718338e92e5332204adbe6a2369d1f6ecee

--- epc/spencer_configs/bokondini.sql
+++ epc/spencer_configs/bokondini.sql
@@ -111,7 +111,7 @@ CREATE TABLE `customers` (
   `imsi` varchar(16) NOT NULL,
   `raw_down` int(10) unsigned DEFAULT '0',
   `raw_up` int(10) unsigned DEFAULT '0',
-  `balance` float(10,2) unsigned DEFAULT '0' COMMENT 'in USD for now',
+  `balance` float(10,2) DEFAULT '0.0',
   `enabled` tinyint(1) DEFAULT '0',
   `msisdn` varchar(16) DEFAULT 'NotUsed',
   PRIMARY KEY (`imsi`)
