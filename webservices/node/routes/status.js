185d0fe6f2cc76e89775cdcf98f5bdf7f737f38f
Spencer Sevilla <sevilla@cs.washington.edu>
2018-05-08 14:45:05 -0700

    node now querying ip lookups
    Former-commit-id: 90540675b43e1980db025c65534e3d819a34aa58

--- webservices/node/routes/status.js
+++ webservices/node/routes/status.js
@@ -3,8 +3,10 @@ var router = express.Router();
 var customer = require('../models/customer');
 
 router.get('/', function(req, res, next) {
-  var imsi = '910540000000999';//req.ip;
-  customer.find(imsi).then((data) => {
+
+  // var ip = req.ip
+  var ip = "192.168.151.6"
+  customer.find_by_ip(ip).then((data) => {
     // console.log(data);
     res.render('status', {
       title: 'Home',
@@ -15,6 +17,20 @@ router.get('/', function(req, res, next) {
   });
 });
   
+// var ip = req.ip
+// imsi.find(ip).retval[0]((retval) => {
+//   customer.find(imsi).then((data) => {
+//     // console.log(data);
+//     res.render('status', {
+//       title: 'Home',
+//       raw_up: data[0].raw_up,
+//       raw_down: data[0].raw_down,
+//       balance: data[0].balance
+//     });
+//   });
+
+// })
+
 // router.post('/transfer', function(req,res) {
 //   var imsi = '910540000000999';
 //   var amount = req.body.amount;
