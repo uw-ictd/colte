de084e66658870d342cfb7a525b859fcc9753210
Spencer Sevilla <sevilla@cs.washington.edu>
2018-05-07 21:26:22 -0700

    adding different routes/views and tabs for them

---
+++ webservices/node/routes/status.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var customer = require('../models/customer');
+
+router.get('/', function(req, res, next) {
+  var imsi = '910540000000999';//req.ip;
+  customer.find(imsi).then((data) => {
+    // console.log(data);
+    res.render('status', {
+      title: 'Home',
+      raw_up: data[0].raw_up,
+      raw_down: data[0].raw_down,
+      balance: data[0].balance
+    });
+  });
+});
+  
+// router.post('/transfer', function(req,res) {
+//   var imsi = '910540000000999';
+//   var amount = req.body.amount;
+//   var msisdn = req.body.msisdn;
+//   customer.transfer_balance(imsi, msisdn, amount).catch((error) => {
+//     console.log("Transfer error: " + error);
+//   });
+//   res.redirect('/user');
+// });
+module.exports = router;
