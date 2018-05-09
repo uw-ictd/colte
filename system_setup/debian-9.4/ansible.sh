30ad3b03a93c96e8c6b87d8e396e99a1221946ae
Spencer Sevilla <sevilla@cs.washington.edu>
2018-05-08 19:10:14 -0700

    major filesystem re-arch to put all the network non-essential services into lte_extras folder
    Former-commit-id: ce698b9b750e6e6da26e26f4636398c9149710a3

--- system_setup/debian-9.4/ansible.sh
+++ system_setup/debian-9.4/ansible.sh
@@ -9,6 +9,22 @@ if [ $COLTE_EPC == 1 ]; then
 	ansible-playbook -K -v -i "localhost," -c local $COLTE_DIR/epc/install_scripts/debian-9.4.yml
 fi
 
+if [ $COLTE_BILLING == 1 ]; then
+	ansible-playbook -K -v -i "localhost," -c local $COLTE_DIR/lte_extras/billing/install_scripts/debian-9.4.yml
+fi
+
+if [ $COLTE_WEBGUI == 1 ]; then
+	ansible-playbook -K -v -i "localhost," -c local $COLTE_DIR/lte_extras/webgui/install_scripts/debian-9.4.yml
+fi
+
+if [ $COLTE_IMS == 1 ]; then
+	ansible-playbook -K -v -i "localhost," -c local $COLTE_DIR/lte_extras/bind/debian-9.4.yml
+fi
+
+if [ $COLTE_IMS == 1 ]; then
+	ansible-playbook -K -v -i "localhost," -c local $COLTE_DIR/lte_extras/ims/debian-9.4.yml
+fi
+
 if [ $COLTE_ENBRAINS == 1 ]; then
 	ansible-playbook -K -v -i "localhost," -c local $COLTE_DIR/enbrains/install_scripts/debian-9.4.yml
 fi
@@ -16,16 +32,3 @@ fi
 if [ $COLTE_EMERGENCY_WEBSERVICES == 1 ]; then
 	ansible-playbook -K -v -i "localhost," -c local $COLTE_DIR/emergency_webservices/install_scripts/debian-9.4.yml
 fi
-
-if [ $COLTE_IMS == 1 ]; then
-	ansible-playbook -K -v -i "localhost," -c local $COLTE_DIR/lte_extras/ims/debian-9.4.yml
-fi
-
-if [ $COLTE_NODE == 1 ]; then
-	ansible-playbook -K -v -i "localhost," -c local $COLTE_DIR/webservices/node/install_scripts/debian-9.4.yml
-fi
-
-if [ $COLTE_BILLING == 1 ]; then
-	ansible-playbook -K -v -i "localhost," -c local $COLTE_DIR/webservices/billing/install_scripts/debian-9.4.yml
-fi
-
