e4637381874367e1a2de5a0a26f7c513dfeef43f
CoLTE Community LTE <colte@colte-epc.cs.washington.edu>
2018-05-11 20:14:53 +0000

    billing and iptables code/watchdog now work great, kick users off and let them back on as necessary

--- lte_extras/billing/iptables_watchdog.py
+++ lte_extras/billing/iptables_watchdog.py
@@ -2,21 +2,6 @@ import MySQLdb
 import os
 import subprocess
 
-db = MySQLdb.connect(host="localhost",
-                     user=os.environ.get('COLTE_USER'),
-                     passwd=os.environ.get('COLTE_DBPASS'),
-		     	 	 db="colte_db")
-cursor = db.cursor()
-
-query = "SELECT imsi, ip FROM customers WHERE bridged=0 AND data_balance>0"
-numrows = cursor.execute(query)
-
-for row in cursor:
-	imsi = row[0]
-	ip = row[1]
-	print "IPTables Watchdog Re-Enabling IMSI " + imsi + "'s access to the Internet"
-	disable_iptables_filter(ip)
-
 #query = "SELECT imsi, ip FROM customers WHERE bridged=1 AND data_balance<=0"
 #numrows = cursor.execute(query)
 
@@ -29,7 +14,7 @@ for row in cursor:
 
 def enable_iptables_filter(c):
 	# "sudo iptables -I FORWARD -s " + c.ip + " -j REJECT"
-	p = subprocess.Popen(["iptables", "-I", "FORWARD", "-s", c.ip, "-j", "REJECT"], stdout=subprocess.PIPE)
+	p = subprocess.Popen(["iptables", "-I", "FORWARD", "-s", ip, "-j", "REJECT"], stdout=subprocess.PIPE)
 	output , err = p.communicate()
 	print output
 	# rule = iptc.Rule()
@@ -38,9 +23,9 @@ def enable_iptables_filter(c):
 	# chain = iptc.Chain(iptc.Table.(iptc.Table.FILTER), "FORWARD")
 	# chain.insert_rule(rule)
 
-def disable_iptables_filter(c):
+def disable_iptables_filter(ip):
 	# "sudo iptables -D FORWARD -s " + c.ip + " -j REJECT"
-	p = subprocess.Popen(["iptables", "-I", "FORWARD", "-s", c.ip, "-j", "REJECT"], stdout=subprocess.PIPE)
+	p = subprocess.Popen(["iptables", "-D", "FORWARD", "-s", ip, "-j", "REJECT"], stdout=subprocess.PIPE)
 	output , err = p.communicate()
 	print output
 	# rule = iptc.Rule()
@@ -48,3 +33,19 @@ def disable_iptables_filter(c):
 	# rule.target = iptc.Target('REJECT')
 	# chain = iptc.Chain(iptc.Table.(iptc.Table.FILTER), "FORWARD")
 	# chain.delete_rule(rule)
+
+db = MySQLdb.connect(host="localhost",
+                     user=os.environ.get('COLTE_USER'),
+                     passwd=os.environ.get('COLTE_DBPASS'),
+		     	 	 db="colte_db")
+cursor = db.cursor()
+
+query = "SELECT customers.imsi, ip FROM customers,static_ips WHERE customers.imsi=static_ips.imsi AND bridged=0 AND data_balance>0"
+numrows = cursor.execute(query)
+
+for row in cursor:
+	imsi = row[0]
+	ip = row[1]
+	print "IPTables Watchdog Re-Enabling IMSI " + imsi + "'s access to the Internet"
+	disable_iptables_filter(ip)
+
