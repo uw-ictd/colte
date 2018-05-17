b93d13a732434bc29612797faf88d49433100429
Spencer Sevilla <sevilla@cs.washington.edu>
2018-05-17 10:45:39 -0700

    getting emergency_webservices rewritten to use nginx
    Former-commit-id: 70b301d101acc2f60a635db75a9e4a728ab1a37a

---
+++ emergency_webservices/registry/polls/admin.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from polls.models import Person
+
+# Register your models here.
+admin.site.register(Person)
+
