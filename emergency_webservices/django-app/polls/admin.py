646aa54f4585d7870c75534365e8753e58f22565
Spencer Sevilla <sevilla@cs.washington.edu>
2018-05-17 10:58:21 -0700

    big emergency website mods
    Former-commit-id: ee947b1edc565f04a636fb2f7566c5c1bf44939d

--- emergency_webservices/django-app/polls/admin.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.contrib import admin
-from polls.models import Person
-
-# Register your models here.
-admin.site.register(Person)
-
