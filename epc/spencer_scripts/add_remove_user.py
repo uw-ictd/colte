eef18edb1f5472adc9e27f3d8b34907bec2fc74f
Spencer Sevilla <sevilla@cs.washington.edu>
2018-07-10 15:26:04 -0700

    improving script
    Former-commit-id: 34e41ea9216b9d455fe5256c1731ca92ff60e4f7

--- epc/spencer_scripts/add_remove_user.py
+++ epc/spencer_scripts/add_remove_user.py
@@ -26,24 +26,29 @@ command = sys.argv[1]
 ############### OPTION ONE: ADD A USER TO THE DATABASE ##################
 #########################################################################
 if (command == "add"):
-	if num_args != 6:  # the program name and the arguments
+	if num_args != 7:  # the program name and the arguments
 	  # stop the program and print an error message
-	  sys.exit("Incorrect number of arguments: format is add imsi msisdn key opc")
+	  sys.exit("Incorrect number of arguments: format is add imsi msisdn ip key opc")
 
 	imsi = sys.argv[2]
 	msisdn = sys.argv[3]
-	key = sys.argv[4]
-	opc = sys.argv[5]
+	msisdn = sys.argv[4]
+	key = sys.argv[5]
+	opc = sys.argv[6]
 
 	commit_str = "INSERT INTO pdn (apn, pgw_id, users_imsi) VALUES ('ltebox', 3, " + imsi + ")"
 	cursor.execute(commit_str)
 	print commit_str
 
 	commit_str = "INSERT INTO users (imsi, msisdn, `key`, OPc, sqn, rand) VALUES ('" + imsi + "', '" + msisdn + "', " + key + ", " + opc + ", 351, 0x0)"
-	print commit_str
 	cursor.execute(commit_str)
+	print commit_str
 
 	commit_str = "INSERT INTO customers (imsi, raw_down, raw_up, data_balance, balance, bridged, enabled, msisdn) VALUES (" + imsi + ", 0, 0, 10000000, 500, 1, 1, '" + msisdn + "')"
+	cursor.execute(commit_str)
+	print commit_str
+
+	commit_str = "INSERT INTO static_ips (imsi, ip) VALUES ('" + imsi + "', '" + ip + "')"
 	cursor.execute(commit_str)
 	print commit_str
 
@@ -69,3 +74,6 @@ if (command == "rm"):
 	cursor.execute(commit_str)
 	print commit_str
 
+	commit_str = "DELETE FROM static_ips WHERE imsi = " + imsi
+	cursor.execute(commit_str)
+	print commit_str
