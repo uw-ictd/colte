0df7cf7193f468c5e549327afc55b4ebb3f5fbd9
Matt Johnson <matt9j@cs.washington.edu>
2018-08-03 16:09:29 +0900

    [billing] Refactor ntopng parse into smaller functions
    Instead of one big monolithic function, this commit splits the logic
    into a separate update of the customer structure and a parse of the
    data file. The customer structure could probably still use some work.

--- lte_extras/billing/import_to_db.py
+++ lte_extras/billing/import_to_db.py
@@ -11,12 +11,12 @@ class Customer(object):
         self.imsi = constants.ZERO_IMSI
         self.ip = constants.ZERO_IPv4
 
-        self.new_raw_down = 0
-        self.new_raw_up = 0
+        self.new_bytes_down = 0
+        self.new_bytes_up = 0
         self.new_data_balance = 0
 
-        self.old_raw_down = 0
-        self.old_raw_up = 0
+        self.old_bytes_down = 0
+        self.old_bytes_up = 0
         self.old_data_balance = 0
 
         self.balance = 0.0
@@ -40,64 +40,74 @@ def parse_ntopng_log_file(filename, db_connection):
             c.new_bytes_up = int(vals[2])
 
             try:
-                c.imsi = db_connection.imsi_from_ip(c.ip)
-            except LookupError as e:
-                logging.error("Failed to lookup imsi for IP: " + c.ip + str(e))
+                fill_customer_structure(c, db_connection)
+            except LookupError:
                 continue
-
-            try:
-                raw_customer_tuple = db_connection.query_customer_info(c.imsi)
-            except LookupError as e:
-                logging.error("Failed to get customer info for imsi: " + c.imsi + str(e))
-                continue
-
-            c.old_bytes_down = int(raw_customer_tuple[0])
-            c.old_bytes_up = int(raw_customer_tuple[1])
-            c.old_data_balance = int(raw_customer_tuple[2])
-            c.balance = raw_customer_tuple[3]
-            c.bridged = raw_customer_tuple[4]
-            c.enabled = raw_customer_tuple[5]
-
-            # sanity check
-            if c.enabled != 1:
-                logging.error("IMSI " + c.imsi + " not set to enabled in customers db? Value is " + str(c.enabled))
-
-            # TODO(matt9j) Check if this is correct. Seems fishy with system restart and poweroff cases.
-            # data is only incremented (cumulatively) so the only way these values will ever be less than previous val
-            # is if the counter reset. hopefully this never happens but edge-cases are important
-            # NOTE: some counters might reset when others don't!
-            if c.new_bytes_down < c.old_bytes_down:
-                bytes_down_in_period = c.new_bytes_down
-            else:
-                bytes_down_in_period = int(c.new_bytes_down) - int(c.old_bytes_down)
-
-            if c.new_bytes_up < c.old_bytes_up:
-                bytes_up_in_period = c.new_bytes_up
-            else:
-                bytes_up_in_period = int(c.new_bytes_up) - int(c.old_bytes_up)
-
-            # SANITY CHECK
-            if (bytes_down_in_period < 0) or (bytes_up_in_period < 0):
-                logging.error("MAJOR BILLING SCRIPT ERROR: HOW COULD WE GET NEGATIVE BYTE USAGE?!?")
-                bytes_down_in_period = 0
-                bytes_up_in_period = 0
-
-            total_bytes_in_period = bytes_down_in_period + bytes_up_in_period
-            c.new_data_balance = c.old_data_balance - total_bytes_in_period
-
-            logging.info("IMSI " + c.imsi + " used " + str(total_bytes_in_period) + " bytes. Bytes_remaining = " + str(c.new_data_balance) + ", raw_down = " + str(c.new_bytes_down) + ", raw_up = " + str(c.new_bytes_up))
 
             verify_balance(c)
 
-            # END: store the record locally and onto the next user
-            new_record = (c.new_bytes_down, c.new_bytes_up, str(c.new_data_balance), c.enabled, c.bridged, c.imsi)
-            record_list.append(new_record)
+            # TODO(matt9j) Abstract this interface better into the DB interface class.
+            # Return the customer record tuple, in a format specific to the database.
+            record_list.append((c.new_bytes_down, c.new_bytes_up, str(c.new_data_balance),
+                                c.enabled, c.bridged, c.imsi))
 
         # Commit all updates at once to save on DB operations
-        # TODO(matt9j) This does not use transactions, so it could be racy with async updates from the web UI
         db_connection.batch_update_customer_info(record_list)
+        # TODO(matt9j) Not sure if prod is setup for transactions, so it could be racy with async updates from the UI
         # TODO(matt9j) How do I know if the commit failed? I need to retry, right?
         db_connection.commit_changes()
+
+
+def fill_customer_structure(customer, db_connection):
+    try:
+        customer.imsi = db_connection.imsi_from_ip(customer.ip)
+    except LookupError as e:
+        logging.error("Failed to lookup imsi for IP: " + customer.ip + str(e))
+        raise e
+
+    try:
+        raw_customer_tuple = db_connection.query_customer_info(customer.imsi)
+    except LookupError as e:
+        logging.error("Failed to get customer info for imsi: " + customer.imsi + str(e))
+        raise e
+
+    customer.old_bytes_down = int(raw_customer_tuple[0])
+    customer.old_bytes_up = int(raw_customer_tuple[1])
+    customer.old_data_balance = int(raw_customer_tuple[2])
+    customer.balance = raw_customer_tuple[3]
+    customer.bridged = raw_customer_tuple[4]
+    customer.enabled = raw_customer_tuple[5]
+
+    # sanity check
+    if customer.enabled != 1:
+        logging.error("IMSI " + customer.imsi + " not set to enabled in customer db? Value is " + str(customer.enabled))
+
+    # TODO(matt9j) Check if this is correct. Seems fishy with system restart and poweroff cases.
+    # data is only incremented (cumulatively) so the only way these values will ever be less than previous val
+    # is if the counter reset. hopefully this never happens but edge-cases are important
+    # NOTE: some counters might reset when others don't!
+    if customer.new_bytes_down < customer.old_bytes_down:
+        bytes_down_in_period = customer.new_bytes_down
+    else:
+        bytes_down_in_period = int(customer.new_bytes_down) - int(customer.old_bytes_down)
+
+    if customer.new_bytes_up < customer.old_bytes_up:
+        bytes_up_in_period = customer.new_bytes_up
+    else:
+        bytes_up_in_period = int(customer.new_bytes_up) - int(customer.old_bytes_up)
+
+    # SANITY CHECK
+    if (bytes_down_in_period < 0) or (bytes_up_in_period < 0):
+        logging.error("MAJOR BILLING SCRIPT ERROR: HOW COULD WE GET NEGATIVE BYTE USAGE?!?")
+        bytes_down_in_period = 0
+        bytes_up_in_period = 0
+
+    total_bytes_in_period = bytes_down_in_period + bytes_up_in_period
+    customer.new_data_balance = customer.old_data_balance - total_bytes_in_period
+
+    logging.info("IMSI " + customer.imsi + " used " + str(total_bytes_in_period) + " bytes. Bytes_remaining = " +
+                 str(customer.new_data_balance) + ", raw_down = " + str(customer.new_bytes_down) + ", raw_up = " +
+                 str(customer.new_bytes_up))
 
 
 def verify_balance(c):
@@ -141,7 +151,11 @@ def out_of_data(c):
     # TODO(sevilla) STEP 2: if they're also out of balance, cut them off entirely (figure this out later)
 
 
-if __name__ == "__main__":
+def main():
     db_connection = db_interface.BillingConnection()
     filename = os.environ.get('COLTE_DIR') + "/lte_extras/billing/tmp_dump.txt"
     parse_ntopng_log_file(filename, db_connection)
+
+
+if __name__ == "__main__":
+    main()
