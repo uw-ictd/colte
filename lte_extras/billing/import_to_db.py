7bda53b8063b264712d0d9bc0c95f4f26cde033f
Matt Johnson <matt9j@cs.washington.edu>
2018-08-03 15:51:29 +0900

    [billing] Refactor database update
    Split the database update and isolate the code that actually touches
    the db to allow for better unit testing with fakes and/or mocks.

--- lte_extras/billing/import_to_db.py
+++ lte_extras/billing/import_to_db.py
@@ -1,7 +1,8 @@
 from helpers import constants
 from helpers import iptables
 
-import MySQLdb
+import db_interface
+import logging
 import os
 
 
@@ -23,109 +24,80 @@ class Customer(object):
         self.enabled = 1
 
 
-def get_imsi_from_ip(ip_addr, cursor):
-    query = "SELECT imsi FROM static_ips WHERE ip = \"" + ip_addr + "\""
-    print(query)
-    numrows = cursor.execute(query)
+def parse_ntopng_log_file(filename, db_connection):
+    with open(filename, 'r') as file:
+        # The first row is just the time of the entry
+        timestr = file.readline().split()
+        print("Read Timestring: " + str(timestr))
 
-    if numrows == 0:
-        print("ERROR: why do we not have an IMSI for ip address " + ip_addr + " in the database?!?!?")
-        return constants.ZERO_IMSI
+        record_list = []
+        for line in file:
+            c = Customer()
 
-    if numrows > 1:
-        print("More than one IMSI entry for same ip address? What happened???")
-        return constants.ZERO_IMSI
+            vals = line.split()
+            c.ip = vals[0]
+            c.new_bytes_down = int(vals[1])
+            c.new_bytes_up = int(vals[2])
 
-    answer_tuple = cursor.fetchone()
-    imsi = answer_tuple[0]
-    return imsi
+            try:
+                c.imsi = db_connection.imsi_from_ip(c.ip)
+            except LookupError as e:
+                logging.error("Failed to lookup imsi for IP: " + c.ip + str(e))
+                continue
 
+            try:
+                raw_customer_tuple = db_connection.query_customer_info(c.imsi)
+            except LookupError as e:
+                logging.error("Failed to get customer info for imsi: " + c.imsi + str(e))
+                continue
 
-def main():
-    record_list = []
-    db = MySQLdb.connect(host="localhost",
-                         user=os.environ.get('COLTE_USER'),
-                         passwd=os.environ.get('COLTE_DBPASS'),
-                         db="colte_db")
-    cursor = db.cursor()
-    filename = os.environ.get('COLTE_DIR') + "/lte_extras/billing/tmp_dump.txt"
-    file = open(filename, 'r')
+            c.old_bytes_down = int(raw_customer_tuple[0])
+            c.old_bytes_up = int(raw_customer_tuple[1])
+            c.old_data_balance = int(raw_customer_tuple[2])
+            c.balance = raw_customer_tuple[3]
+            c.bridged = raw_customer_tuple[4]
+            c.enabled = raw_customer_tuple[5]
 
-    # FIRST ROW IS JUST THE TIME OF THE ENTRY
-    timestr = file.readline().split()
-    print("Read Timestring: " + str(timestr))
+            # sanity check
+            if c.enabled != 1:
+                logging.error("IMSI " + c.imsi + " not set to enabled in customers db? Value is " + str(c.enabled))
 
-    for line in file:
-        c = Customer()
+            # TODO(matt9j) Check if this is correct. Seems fishy with system restart and poweroff cases.
+            # data is only incremented (cumulatively) so the only way these values will ever be less than previous val
+            # is if the counter reset. hopefully this never happens but edge-cases are important
+            # NOTE: some counters might reset when others don't!
+            if c.new_bytes_down < c.old_bytes_down:
+                bytes_down_in_period = c.new_bytes_down
+            else:
+                bytes_down_in_period = int(c.new_bytes_down) - int(c.old_bytes_down)
 
-        vals = line.split()
-        c.ip = vals[0]
-        c.new_bytes_down = vals[1]
-        c.new_bytes_up = vals[2]
+            if c.new_bytes_up < c.old_bytes_up:
+                bytes_up_in_period = c.new_bytes_up
+            else:
+                bytes_up_in_period = int(c.new_bytes_up) - int(c.old_bytes_up)
 
-        c.imsi = get_imsi_from_ip(c.ip, cursor)
-        if c.imsi == constants.ZERO_IMSI:
-            continue
+            # SANITY CHECK
+            if (bytes_down_in_period < 0) or (bytes_up_in_period < 0):
+                logging.error("MAJOR BILLING SCRIPT ERROR: HOW COULD WE GET NEGATIVE BYTE USAGE?!?")
+                bytes_down_in_period = 0
+                bytes_up_in_period = 0
 
-        query = "SELECT raw_down, raw_up, data_balance, balance, bridged, enabled FROM customers WHERE imsi = " + c.imsi 
-        numrows = cursor.execute(query)
+            total_bytes_in_period = bytes_down_in_period + bytes_up_in_period
+            c.new_data_balance = c.old_data_balance - total_bytes_in_period
 
-        if numrows == 0:
-            print("ERROR: why do we not have info for this imsi in the database?!?!?")
-            continue
+            logging.info("IMSI " + c.imsi + " used " + str(total_bytes_in_period) + " bytes. Bytes_remaining = " + str(c.new_data_balance) + ", raw_down = " + str(c.new_bytes_down) + ", raw_up = " + str(c.new_bytes_up))
 
-        if numrows > 1:
-            print("More than one entry for same imsi? What happened???")
-            continue
+            verify_balance(c)
 
-        answer_tuple = cursor.fetchone()
+            # END: store the record locally and onto the next user
+            new_record = (c.new_bytes_down, c.new_bytes_up, str(c.new_data_balance), c.enabled, c.bridged, c.imsi)
+            record_list.append(new_record)
 
-        c.old_bytes_down = answer_tuple[0]
-        c.old_bytes_up = answer_tuple[1]
-        c.old_data_balance = answer_tuple[2]
-
-        c.balance = answer_tuple[3]
-        c.bridged = answer_tuple[4]
-        c.enabled = answer_tuple[5]
-
-        # sanity check
-        if c.enabled != 1:
-            print("ERROR: Why is IMSI " + c.imsi + " not set to enabled in customers db? Value is " + str(c.enabled))
-
-        # data is only incremented (cumulatively, duh) so the only way these values will ever be less than previous val
-        # is if the counter reset. hopefully this never happens but edge-cases are important
-        # NOTE: some counters might reset when others don't!
-        if c.new_bytes_down < c.old_bytes_down:
-            bytes_down_in_period = c.new_bytes_down
-        else:
-            bytes_down_in_period = int(c.new_bytes_down) - int(c.old_bytes_down)
-
-        if c.new_bytes_up < c.old_bytes_up:
-            bytes_up_in_period = c.new_bytes_up
-        else:
-            bytes_up_in_period = int(c.new_bytes_up) - int(c.old_bytes_up)
-
-        # SANITY CHECK
-        if (bytes_down_in_period < 0) or (bytes_up_in_period < 0):
-            print("MAJOR BILLING SCRIPT ERROR: HOW COULD WE GET NEGATIVE BYTE USAGE?!?")
-            bytes_down_in_period = 0
-            bytes_up_in_period = 0
-
-        total_bytes_in_period = bytes_down_in_period + bytes_up_in_period
-        c.new_data_balance = c.old_data_balance - total_bytes_in_period
-
-        print("IMSI " + c.imsi + " used " + str(total_bytes_in_period) + " bytes. Bytes_remaining = " + str(c.new_data_balance) + ", raw_down = " + str(c.new_bytes_down) + ", raw_up = " + str(c.new_bytes_up))
-
-        verify_balance(c)
-
-        # END: store the record locally and onto the next user
-        new_record = (c.new_bytes_down, c.new_bytes_up, str(c.new_data_balance), c.enabled, c.bridged, c.imsi)
-        record_list.append(new_record)
-
-    # (commit all updates at once to save on DB operations)
-    commit_str = "UPDATE customers SET raw_down = %s, raw_up = %s, data_balance = %s, enabled = %s, bridged = %s WHERE imsi = %s"
-
-    cursor.executemany(commit_str, record_list)
+        # Commit all updates at once to save on DB operations
+        # TODO(matt9j) This does not use transactions, so it could be racy with async updates from the web UI
+        db_connection.batch_update_customer_info(record_list)
+        # TODO(matt9j) How do I know if the commit failed? I need to retry, right?
+        db_connection.commit_changes()
 
 
 def verify_balance(c):
@@ -146,30 +118,30 @@ def verify_balance(c):
 
 
 def alert_crossed_10mb(c):
-    print("IMSI " + c.imsi + " has less than 10MB of data remaining.")
+    # TODO(sevilla) Once ims is working, send a message to the UE
+    logging.info("IMSI " + c.imsi + " has less than 10MB of data remaining.")
 
 
 def alert_crossed_5mb(c):
-    print("IMSI " + c.imsi + " has less than 5 MB of data remaining.")
+    # TODO(sevilla) Once ims is working, send a message to the UE
+    logging.info("IMSI " + c.imsi + " has less than 5 MB of data remaining.")
 
 
 def alert_crossed_1mb(c):
-    print("IMSI " + c.imsi + " has less than 1 MB of data remaining.")
+    # TODO(sevilla) Once ims is working, send a message to the UE
+    logging.info("IMSI " + c.imsi + " has less than 1 MB of data remaining.")
 
 
 def out_of_data(c):
-    # STEP 1: enable iptables filter to ensure that they can't get out on the general Internet
-    print("IMSI " + c.imsi + " out of data, enabling iptables filter.")
+    # TODO(sevilla) Once ims is working, send a message to the UE
+    logging.info("IMSI " + c.imsi + " out of data, enabling iptables filter.")
     iptables.enable_forward_filter(c.ip)
     c.bridged = 0
 
-    # STEP 2: if they're also out of balance, cut them off entirely (figure this out later)
-    if c.balance <= 0:
-        # HERE: they're out of data balance AND out of money. Cut them off completely!
-        print("IMSI " + c.imsi + " also out of money, removing from network.")
-        # hss_disable_user(imsi)
-        # enabled = "0"
+    # TODO(sevilla) STEP 2: if they're also out of balance, cut them off entirely (figure this out later)
 
 
 if __name__ == "__main__":
-    main()
+    db_connection = db_interface.BillingConnection()
+    filename = os.environ.get('COLTE_DIR') + "/lte_extras/billing/tmp_dump.txt"
+    parse_ntopng_log_file(filename, db_connection)
