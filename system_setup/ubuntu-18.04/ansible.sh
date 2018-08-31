77c34df3b84f8031ab3688f90816417fe59359fc
vagrant <vagrant@vagrant.vm>
2018-08-31 16:58:34 +0000

    preliminary build script for 1804 works great. NOTE: everything compiles/installs but NOT TESTED
    Former-commit-id: db98ed135bf09b46e080cc323ed9aaef1c31fb86

--- system_setup/ubuntu-18.04/ansible.sh
+++ system_setup/ubuntu-18.04/ansible.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -K -v -i "localhost," -c local $COLTE_DIR/system_setup/debian-9.4/play.yml
+ansible-playbook -K -v -i "localhost," -c local $COLTE_DIR/system_setup/ubuntu-18.04/play.yml
