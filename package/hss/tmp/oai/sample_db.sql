055594c769594bf60730cfe5c7bcf67274583601
Spencer Sevilla <sevilla@cs.washington.edu>
2018-09-09 21:40:46 -0700

    minor typo fix
    Former-commit-id: cb283aab8cdb045bc291997ba515fb4d544ff4a2

--- package/hss/tmp/oai/sample_db.sql
+++ package/hss/tmp/oai/sample_db.sql
@@ -1,4 +1,4 @@
-:-- MySQL dump 10.13  Distrib 5.7.18, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.18, for Linux (x86_64)
 --
 -- Host: localhost    Database: colte_db
 -- ------------------------------------------------------
