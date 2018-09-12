0f84bbba652b19ed2e36f35fa6a0ec8b0574e33a
Spencer Sevilla <sevilla@cs.washington.edu>
2018-09-11 18:51:41 -0700

    minor script fixes
    Former-commit-id: e7030c4a7325e7253796243ec3dd0d6717fd1b94

--- system_setup/ansible.sh
+++ system_setup/ansible.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -K -v -i "localhost," -c local $COLTE_DIR/system_setup/ubuntu-18.04/play.yml
+ansible-playbook -K -v -i "localhost," -c local $COLTE_DIR/system_setup/play.yml
