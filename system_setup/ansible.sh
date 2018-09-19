58677e54ccb971758398f97955d23f70a4b9353c
Spencer Sevilla <sevilla@cs.washington.edu>
2018-09-19 09:22:12 -0700

    major push to simplify ansible scripts
    Former-commit-id: 3eb9846ad4efbba9c5c2acb267e9a0d713ad7865

--- system_setup/ansible.sh
+++ system_setup/ansible.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+export COLTE_DIR=$BASEDIR
+export COLTE_USER=$USER
 ansible-playbook -K -v -i "localhost," -c local $COLTE_DIR/system_setup/play.yml
