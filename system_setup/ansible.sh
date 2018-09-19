af5a484bef12a9651bfd7e1740d22e5292a1eb6b
vagrant <vagrant@vagrant.vm>
2018-09-19 21:11:02 +0000

    many bugfixes but rearch now runs fine
    Former-commit-id: 010be317ae2509c3d7151fea7b4b54b5917add41

--- system_setup/ansible.sh
+++ system_setup/ansible.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
-export COLTE_DIR=$BASEDIR
-export COLTE_USER=$USER
-ansible-playbook -K -v -i "localhost," -c local $COLTE_DIR/system_setup/play.yml
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ansible-playbook -K -v -i "localhost," -c local $BASEDIR/play.yml
