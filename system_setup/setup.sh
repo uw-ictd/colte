21b05e7624b02da6190d46267a87f29e61adbdd8
vagrant <vagrant@vagrant.vm>
2018-09-19 21:11:02 +0000

    many bugfixes but rearch now runs fine
    Former-commit-id: 010be317ae2509c3d7151fea7b4b54b5917add41

--- system_setup/setup.sh
+++ system_setup/setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
-export COLTE_DIR=$BASEDIR
-export COLTE_USER=$USER
-
 sudo apt-get update
 sudo apt-get install -y ansible
 ansible-playbook -K -v -i "localhost," -c local $COLTE_DIR/system_setup/play.yml
