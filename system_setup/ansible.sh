d60e7f54986f2d9da893df10bd82f0ec50af4185
Spencer Sevilla <sevilla@cs.washington.edu>
2018-09-19 16:55:59 -0700

    install scripts/plays now integrated
    Former-commit-id: 181ca373b5fe7f6368b0ab314c442529c6f71e81

--- system_setup/ansible.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ansible-playbook -K -v -i "localhost," -c local $BASEDIR/play.yml
---
+++ system_setup/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ "$#" -ne 1 ]; then
+    echo "Error: Incorrect args. Usage is ./install.sh {basic_install|full_install|reconfigure}"
+    exit -1
+fi
+
+if [ ! -f $BASEDIR/$1.yml ]; then
+    echo "Error: Invalid play! Usage is ./install.sh {playbook name}."
+    echo "Available options include basic_install, full_install, source_build, and reconfigure."
+    echo "To add a play, create a .yml file in this directory."
+    exit -1
+fi
+
+sudo apt-get update
+sudo apt-get install -y ansible
+ansible-playbook -K -v -i "localhost," -c local $BASEDIR/$1.yml
