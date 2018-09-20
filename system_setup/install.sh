4397c5ad49081f30ae58a211192204c392a4202a
Spencer Sevilla <sevilla@cs.washington.edu>
2018-09-19 17:33:34 -0700

    folded common into system and re-did meta dependencies
    Former-commit-id: e6702092422bf6acac26dc769f00a9943c9df473

--- system_setup/install.sh
+++ system_setup/install.sh
@@ -8,7 +8,7 @@ fi
 
 if [ ! -f $BASEDIR/$1.yml ]; then
     echo "Error: Invalid play! Usage is ./install.sh {playbook name}."
-    echo "Available options include basic_install, full_install, source_build, and reconfigure."
+    echo "Available options include basic_install, full_install, webservices, reconfigure, and source_build."
     echo "To add a play, create a .yml file in this directory."
     exit -1
 fi
