d38e3968b0d9a3cec780820308e5c58a278fc2fd
Spencer Sevilla <sevilla@cs.washington.edu>
2018-09-27 13:04:35 -0700

    major refactor to push more deps/setup into deb packagess

--- system_setup/install.sh
+++ system_setup/install.sh
@@ -2,13 +2,14 @@
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ "$#" -ne 1 ]; then
-    echo "Error: Incorrect args. Usage is ./install.sh {basic_install|full_install|reconfigure}"
-    exit -1
+	sudo apt-get install -y ansible
+	ansible-playbook -K -v -i "localhost," -c local $BASEDIR/source_build.yml
+	exit 0
 fi
 
 if [ ! -f $BASEDIR/$1.yml ]; then
     echo "Error: Invalid play! Usage is ./install.sh {playbook name}."
-    echo "Available options include basic_install, full_install, webservices, reconfigure, and source_build."
+    echo "Available sample options include basic_install, full_install, webservices, reconfigure, and source_build."
     echo "To add a play, create a .yml file in this directory."
     exit -1
 fi
