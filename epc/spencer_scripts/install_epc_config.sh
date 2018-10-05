0a975123220889359e0d561ba4a7b10662725a6c
Spencer Sevilla <sevilla@cs.washington.edu>
2018-10-05 13:05:26 -0700

    more updates

--- epc/spencer_scripts/install_epc_config.sh
+++
@@ -1,7 +0,0 @@
-cd ..
-sudo mkdir -p /usr/local/etc/oai
-sudo cp -rp spencer_configs/*.conf /usr/local/etc/oai/
-sudo cp -rp spencer_configs/freeDiameter /usr/local/etc/oai/
-source oaienv
-./scripts/check_hss_s6a_certificate /usr/local/etc/oai/freeDiameter
-./scripts/check_mme_s6a_certificate /usr/local/etc/oai/freeDiameter
