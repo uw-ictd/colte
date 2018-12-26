5b18c5602bf9a4fe37d340a0388f8df620d6b6d7
Jenny Liang <jennyliang@Jennys-MacBook-Pro.local>
2018-12-26 14:50:30 -0800

    Made raw uploaded column more readable

--- webadmin/routes/users.js
+++ webadmin/routes/users.js
@@ -58,6 +58,7 @@ router.get('/:page', function(req, res, next) {
 
     for (let i = 0; i < data.data.length; i++) {
       data.data[i].raw_down = app.convertBytes(data.data[i].raw_down)
+      data.data[i].raw_up = app.convertBytes(data.data[i].raw_up)
     }
 
     res.render('users', { 
