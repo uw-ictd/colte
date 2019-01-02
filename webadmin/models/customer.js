30d3c5a6ce71b3239819c33d6ae8a694047d48b5
jliang9 <pandagrl101@gmail.com>
2019-01-02 15:20:06 -0800

    Add username column in users.js (#31)
    Updated webadmin to show usernames and make it possible to edit them. Also updated colte-db requirement to 0.9.11 to reflect new database changes (username field). Closes #29

--- webadmin/models/customer.js
+++ webadmin/models/customer.js
@@ -11,11 +11,11 @@ var customer = {
     //     console.log(paginator.data);
     // });
 
-    return knex.select('imsi', 'msisdn', 'raw_down', 'raw_up', 'balance', 'data_balance', 'enabled', 'bridged', 'admin').from('customers').paginate(10, page, true);
+    return knex.select('imsi', 'msisdn', 'raw_down', 'raw_up', 'balance', 'data_balance', 'enabled', 'bridged', 'admin', 'username').from('customers').paginate(10, page, true);
   },
 
   find_by_ip(ip) {
-    return knex.select('customers.imsi', 'raw_up', 'raw_down', 'balance', 'data_balance', 'msisdn', 'admin')
+    return knex.select('customers.imsi', 'raw_up', 'raw_down', 'balance', 'data_balance', 'msisdn', 'admin', 'username')
 		.from('customers').join('static_ips', "customers.imsi", "=", "static_ips.imsi")
 		.where('static_ips.ip', ip);
   },
@@ -24,8 +24,8 @@ var customer = {
     return knex.select('imsi', 'raw_up', 'raw_down', 'balance', 'data_balance', 'msisdn', 'bridged', 'enabled').where('imsi', imsi).from('customers');
   },
 
-  update(imsi, bridged, enabled, balance, data_balance) {
-      return knex.update({balance: balance, data_balance: data_balance, bridged: bridged, enabled: enabled}).where('imsi', imsi).from('customers')
+  update(imsi, bridged, enabled, balance, data_balance, username) {
+      return knex.update({balance: balance, data_balance: data_balance, bridged: bridged, enabled: enabled, username: username}).where('imsi', imsi).from('customers')
       .catch(function(error) {
         throw new Error(error.sqlMessage);
       });
