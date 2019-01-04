2caae4b619ba27ff7962bd6916bd10b44797843c
Jenny Liang <jennyliang@Jennys-MacBook-Pro.local>
2019-01-04 01:14:09 -0800

    Settings panel WIP

---
+++ webadmin/routes/settings.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+var app = require('../app');
+
+router.get('/', function(req, res, next) {
+  res.redirect('settings/network');
+});
+
+router.get('/network', function(req, res, next) {
+  res.render('settings', {
+    translate: app.translate,
+    title: app.translate("Settings"),
+    subtitle: app.translate("System Settings"),
+    network: "active"
+  });
+});
+
+router.get('/epc', function(req, res, next) {
+  res.render('settings', {
+    translate: app.translate,
+    title: app.translate("Settings"),
+    subtitle: app.translate("ePC Settings"),
+    epc: "active"
+  });
+});
+
+router.get('/running-services', function(req, res, next) {
+  res.render('settings', {
+    translate: app.translate,
+    title: app.translate("Settings"),
+    subtitle: app.translate("Running Services Settings"),
+    runningServices: "active"
+  });
+});
+
+router.get('/web-services', function(req, res, next) {
+  res.render('settings', {
+    translate: app.translate,
+    title: app.translate("Settings"),
+    subtitle: app.translate("Web Services Settings"),
+    webServices: "active"
+  });
+});
+
+module.exports = router;
