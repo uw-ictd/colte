3e74e32d9637544c63e0d2ddd16c16c0e5c02131
Jenny Liang <jennyliang@Jennys-MacBook-Pro.local>
2019-01-04 16:27:08 -0800

    Populating data for network and epc settings

--- webadmin/routes/settings.js
+++ webadmin/routes/settings.js
@@ -1,17 +1,46 @@
 var express = require('express');
 var router = express.Router();
 var app = require('../app');
+var handlebars = require('hbs');
+
+const yaml = require('js-yaml');
+const fs   = require('fs');
+
+var configFile = process.env.SETTINGS_VARS;
+var vars = yaml.safeLoad(fs.readFileSync(configFile, 'utf8'));
+
+handlebars.registerHelper('ifTrue', function(value, options) {
+  if(value == true) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
 
 router.get('/', function(req, res, next) {
   res.redirect('settings/network');
 });
 
+router.post('/', function(req, res, next) {
+  
+});
+
+// the variables file lives at /usr/local/etc/colte/config.yml,
+// and you can run the configuration script by running “colteconf update”.
+
+// vars: package/colteconf/usr/bin/colte/roles/configure/vars/main.yml
+// script: package/colteconf/usr/bin/colte/roles/configure/tasks/main.yml
 router.get('/network', function(req, res, next) {
   res.render('settings', {
     translate: app.translate,
     title: app.translate("Settings"),
     subtitle: app.translate("System Settings"),
-    network: "active"
+    network: "active",
+
+    networkName: vars["network_name"],
+    enbInterface: vars["enb_iface"],
+    enbInterfaceAddress: vars["enb_iface_addr"],
+    wanInterface: vars["wan_iface"],
+    lteSubnet: vars["lte_subnet"]
   });
 });
 
@@ -20,7 +49,16 @@ router.get('/epc', function(req, res, next) {
     translate: app.translate,
     title: app.translate("Settings"),
     subtitle: app.translate("EPC Settings"),
-    epc: "active"
+    epc: "active",
+
+    maxEnb: vars["max_enb"],
+    maxUe: vars["max_ue"],
+    plmn: vars["plmn"],
+    local_dns: vars["local_dns"],
+    dnssec: vars["dnssec"],
+    dns: vars["dns"],
+    maxUl: vars["max_dl"],
+    maxDl: vars["max_dl"]
   });
 });
 
@@ -29,7 +67,12 @@ router.get('/running-services', function(req, res, next) {
     translate: app.translate,
     title: app.translate("Settings"),
     subtitle: app.translate("Running Services Settings"),
-    runningServices: "active"
+    runningServices: "active",
+
+    epc: vars["epc"],
+    haualge: vars["haulage"],
+    webGui: vars["web_gui"],
+    webServices:["web_services"]
   });
 });
 
