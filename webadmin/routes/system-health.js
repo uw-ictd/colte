96f0d67347a16bba864a9f622bfedadaa63b3b79
Jenny Liang <jennyliang@D-10-18-226-100.dhcp4.washington.edu>
2019-02-11 18:47:06 -0800

    Overivew page

--- webadmin/routes/system-health.js
+++ webadmin/routes/system-health.js
@@ -7,6 +7,7 @@ const shell = require('shelljs');
 
 const SERVICES_FILE = process.env.SERVICES_HEALTH_INFO;
 const SEARCH_FILE = process.env.SERVICES_SEARCH_INFO;
+const SYSTEM_HEALTH_FILE = process.env.SYSTEM_HEALTH_INFO;
 const NUM_SERVICES = 5;
 
 router.get('/', function(req, res, next) {
@@ -41,7 +42,6 @@ router.post('/services', function(req, res, next) {
 
     // });
 
-    console.log(command);
     getStats(SEARCH_FILE, (err, data) => {
         if (err) {
             throw err;
@@ -58,12 +58,95 @@ router.post('/services', function(req, res, next) {
 });
 
 router.get('/overview', function(req, res, next) {
-    res.render('system-health', {
-        translate: app.translate,
-        title: app.translate("System Health"),
-        services: "active",
+    const cpu = ["model name", "cpu MHz", "cache size", "cpu cores"];
+    const memory = ["MemTotal", "MemFree", "MemAvailable", "Active", "Inactive"];
+    const disk = ['/'];
+
+    shell.exec('../scripts/bash/system-health.sh', (code, stdout, stderr) => { 
+        getStats(SYSTEM_HEALTH_FILE, (err, data) => {
+            if (err) {
+                throw err;
+            }
+            var cpuData = [];
+            var memoryData = [];
+            var diskData = [];
+
+            var dataAsString = data.toString();
+            var delimited = dataAsString.split("-----");
+            
+            cpuData = getCpuMemoryData(delimited[0], cpu);
+            memoryData = getCpuMemoryData(delimited[1], memory);
+            diskData = getDiskData(delimited[2], disk);
+
+            res.render('system-health', {
+                translate: app.translate,
+                title: app.translate("System Health"),
+                services: "active",
+
+                cpuData: cpuData,
+                memoryData: memoryData,
+                diskData: diskData
+            });
+        });
     });
 });
+
+var getCpuMemoryData = (all, filter) => {
+    var data = all.split("\n");
+    var matched = [];
+
+    data.forEach((element) => {
+        if (element !== '') {
+            var line = element.split(":");
+            var type = capitalize(line[0].trim().replace("_", " "));
+            var value = line[1].trim();
+            if (filter.includes(line[0].trim())) {
+                matched.push({type: type, value: value});
+            }
+        }
+    });
+
+    return matched;
+}
+
+var getDiskData = (all, filter) => {
+    var data = all.split("\n");
+    var matched = [];
+
+    for(let i = 1; i < data.length; i++) {
+        var line = data[i].split((/\s+/));
+        if (filter.includes(line[5].trim())) {
+            var obj = {
+                filesystem: line[0].trim(),
+                blocks: line[1].trim(),
+                used: line[2].trim(),
+                available: line[3].trim(),
+                usePercent: line[4].trim(),
+                mounted: line[5].trim()
+            }
+            matched.push(obj);
+        }
+    }
+    console.log(matched);
+
+    return matched;
+}
+
+var capitalize = (str) => {
+    var words = str.split(" ");
+    var result = "";
+
+    words.forEach(element => {
+        var temp = element.trim();
+        if (temp.toLowerCase() === 'id' || temp === 'cpu') {
+            result += (temp.toUpperCase() + " ");
+        } else {
+            result += (temp.charAt(0).toUpperCase() + temp.substring(1) + " ");
+        }
+    });
+
+    return result.trim();
+}
 
 var getPageData = (data, toRender) => {
     var dataAsString = data.toString();
