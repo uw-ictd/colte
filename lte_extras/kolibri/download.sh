8bba2c88b3e8e90c924bda25ec58d39b0b651d8d
CoLTE Community LTE <colte@colte.colte>
2019-04-05 20:32:58 +0000

    Bug fix in the script

--- lte_extras/kolibri/download.sh
+++ lte_extras/kolibri/download.sh
@@ -1,4 +1,4 @@
-TEMP_DEB="$(mktemp)" && wget -O "$TEMP_DEB" 'https://learningequality.org/r/kolibri-deb-latest' && sudo  dpkg -i "TEMP_DEB"
+TEMP_DEB="$(mktemp)" && wget -O "$TEMP_DEB" 'https://learningequality.org/r/kolibri-deb-latest' && sudo  dpkg -i "$TEMP_DEB"
 rm -f "$TEMP_DEB"
 # After running through the initial setup, run kolibri status and log on to the server to complete first time setup
 # To download videos run videos.sh
