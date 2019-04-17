5279ae625e07710654e4765f395b3a431b897237
Rishi Jha <rjha18@live.com>
2019-04-16 17:27:26 -0700

    Some Cleaning

--- webservices/routes/home.js
+++ webservices/routes/home.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var customer = require('../models/customer');
 var app = require('../app');
 var exec = require('child_process').exec;
 const CHECK_STATUS = 0;
@@ -28,7 +27,11 @@ router.post('/checkStatus', function(req, res, next) {
         res.status(200).send("enabled");
       }
     } else {
-      res.status(200).send("disabled");
+      if (out == "enabled") {
+        res.status(200).send("enabled");
+      } else {
+        res.status(200).send("disabled");
+      }
     }
   });
 });
@@ -49,63 +52,5 @@ router.post('/updateStatus', function(req, res, next) {
     }
   });
 });
-
-router.post('/transfer', function(req,res) {
-  var source = req.body.source;
-  var dest = req.body.dest;
-  var amount = req.body.amount;
-
-  console.log("TRANSFER source=" + source + " dest=" + dest + " amount=" + amount);
-  customer.transfer_balance(source, dest, amount).catch((error) => {
-    console.log("Transfer Error: " + error);
-  })
-  .then(function() {
-    res.redirect('/home');
-  });
-});
-
-router.post('/topup', function(req,res) {
-  var imsi = req.body.imsi;
-  var amount = req.body.amount;
-
-  if (amount < 0) {
-    console.log("NEGATIVE!!!");
-  }
-
-  console.log("TOPUP imsi=" + imsi + ", amount =" + amount);
-  customer.top_up(imsi, amount).catch((error) => {
-    console.log("Transfer Error: " + error);
-  })
-  .then(function() {
-    res.redirect('/home');
-  });
-});
-
-router.post('/details', function(req, res, next) {
-  var imsi = req.body.imsi;
-
-  console.log("DETAILS" + imsi);
-  res.redirect('/details/' + imsi);
-});
-
-function getCall(service, status) {
-  if (service == "kolibri") {
-    if (status == CHECK_STATUS) {
-      return "sudo kolibri status";
-    } else if (status == ENABLE) {
-      return "sudo kolibri start";
-    } else {
-      return "sudo kolibri stop";
-    }
-  } else {
-    if (status == CHECK_STATUS) {
-      return "echo sudo systemctl is-enabled " + service;
-    } else if (status == ENABLE) {
-      return "sudo systemctl enable " + service
-    } else {
-      return "sudo systemctl disable " + service
-    }
-  }
-}
 
 module.exports = router;
