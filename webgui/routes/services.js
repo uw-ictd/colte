2c47319bc837ee1ddb3372ef69475d367cfe58ec
Jenny Liang <jliang9@uw.edu>
2019-10-16 00:59:29 -0700

    Added code to render available services

--- webgui/routes/services.js
+++ webgui/routes/services.js
@@ -3,6 +3,30 @@ var router = express.Router();
 var customer = require('../models/customer');
 var app = require('../app');
 
+/* Gets services currently enabled by admin, as specified in 
+/usr/local/etc/colte/services.json. Format of JSON file should be: 
+{
+  serviceName: {
+    enabled: boolean,
+    url: string
+  }, 
+  ...
+}*/
+function getEnabledServices(servicesObject) {
+  var array = [];
+
+  for (var service in servicesObject) {
+    if (servicesObject[service].enabled) {
+      array.push({
+        serviceName: service,
+        url: servicesObject[service].url
+      });
+    }
+  } 
+
+  return array;
+}
+
 router.get('/', function(req, res, next) {
 
   var ip = app.generateIP(req.ip);
@@ -14,10 +38,16 @@ router.get('/', function(req, res, next) {
     // raw_down_str = convertBytes(data[0].raw_down);
     // data_balance_str = convertBytes(data[0].data_balance);
 
+    var services = process.env.ENABLED_SERVICES === "" ? 
+        {} : JSON.parse(process.env.ENABLED_SERVICES);
+
+    var enabledServices = getEnabledServices(services);
+
     res.render('services', {
       translate: app.translate,
       title: app.translate("Home"),
       admin: data[0].admin,
+      services: enabledServices
       // raw_up_str: raw_up_str,
       // raw_down_str: raw_down_str,
       // balance: data[0].balance,
