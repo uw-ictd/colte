ae455ea70577117fb5bab4a28c3fd846023bfddf
Jenny Liang <jliang9@uw.edu>
2020-02-06 17:34:26 -0800

    Fixing permissions issues

--- conf/colteconf.sh
+++ conf/colteconf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /etc/colte/colteconf
 sudo virtualenv env
-sudo source env/bin/activate
-sudo pip install ruamel.yaml
+source env/bin/activate
+pip install ruamel.yaml
 python colteconf.py
 exit
