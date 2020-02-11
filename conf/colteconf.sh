57434b10f0b210cb20b9e50bb508999baaffa089
Jenny Liang <jliang9@uw.edu>
2020-02-11 12:59:19 -0800

    Fixing errors in shell script

--- conf/colteconf.sh
+++ conf/colteconf.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-if ["$EUID" -ne 0]; then
+if [ "$EUID" -ne 0 ]; then
     echo "colteconf: Must run as root!"
     exit 1;
-else
-    cd /etc/colte/colteconf
-    sudo virtualenv env
-    source env/bin/activate
-    sudo pip install ruamel.yaml
-    sudo pip install netaddr
-    python colteconf.py
 fi
+
+cd /etc/colte/colteconf
+sudo virtualenv env
+source env/bin/activate
+sudo pip install ruamel.yaml
+sudo pip install netaddr
+python colteconf.py
