730957a43b58e40ca6b46e817ba58ef116182e9c
Jenny Liang <jliang9@uw.edu>
2020-02-13 13:37:47 -0800

    Fixing bugs

--- conf/colteconf.sh
+++ conf/colteconf.sh
@@ -4,7 +4,7 @@ RED='\033[0;31m'
 NC='\033[0m'
 
 if [ "$EUID" -ne 0 ]; then
-    printf "colteconf: ${RED}error:${NC} Must run as root!"
+    printf "colteconf: ${RED}error:${NC} Must run as root! \n"
     exit 1;
 fi
 
