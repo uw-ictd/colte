8d6af44842880348ac7122c30d95e1e21758192b
Matt Johnson <matt9j@cs.washington.edu>
2021-02-22 08:52:37 -0800

    common-models webgui: Add temporary database test plumbing

--- webgui/tests/purchase.int.test.js
+++ webgui/tests/purchase.int.test.js
@@ -1,27 +1,34 @@
 const test_request = require('supertest');
 const app = require('../app');
+const Knex = require('colte-common-models').TestKnex;
 const purchase_router = require('../routes/purchase');
 
+const databaseName = `colte_test_for_worker_${process.env.JEST_WORKER_ID}`;
+
 beforeAll(async () => {
-  ;
+  Knex.raw(`DROP DATABASE IF EXISTS ${databaseName}`);
+  Knex.raw(`CREATE DATABASE ${databaseName}`);
+  // TODO(matt9j) Add support for creating a clean test DB, possibly from knex
+  // migrations and seeds.
 });
 
 afterAll(async () =>{
-  ;
+  Knex.raw(`DROP DATABASE ${databaseName}`);
+  Knex.destroy();
 })
 
 describe ("purchase API", function() {
-  it('Get main page, no db', async (done) => {
+  it('Get main page', async (done) => {
     const res = await test_request(app)
       .get("/purchase")
-      .set('X-Forwarded-For', '192.168.2.1');
-    expect(res.statusCode).toEqual(503);
+      .set('X-Forwarded-For', '192.168.151.2');
+    expect(res.statusCode).toEqual(200);
     done();
   })
   it('Post main page', async (done) => {
     const res = await test_request(app)
       .post("/purchase")
-      .set('X-Forwarded-For', '192.168.2.1')
+      .set('X-Forwarded-For', '192.168.151.2')
       .send("fish");
     expect(res.statusCode).toEqual(405);
     done();
@@ -29,7 +36,7 @@ describe ("purchase API", function() {
   it('Post main page', async (done) => {
     const res = await test_request(app)
       .get("/purchase/purchase")
-      .set('X-Forwarded-For', '192.168.2.1');
+      .set('X-Forwarded-For', '192.168.151.2');
     expect(res.statusCode).toEqual(405);
     done();
   })
