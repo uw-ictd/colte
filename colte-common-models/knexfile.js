c91926ea68e77e7e0de9f3a971a31cfd4f656dc7
Matt Johnson <matt9j@cs.washington.edu>
2021-02-21 17:32:42 -0800

    common-models: Add a test db configuration

--- colte-common-models/knexfile.js
+++ colte-common-models/knexfile.js
@@ -2,6 +2,16 @@
 
 module.exports = {
 
+  test: {
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    }
+  },
+
   development: {
     client: 'mysql',
     connection: {
