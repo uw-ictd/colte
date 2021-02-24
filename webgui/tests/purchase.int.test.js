cb4ede23f4d942c308daf002dbbcaef562eaae69
Matt Johnson <matt9j@cs.washington.edu>
2021-02-23 18:55:36 -0800

    webgui: Add stress consistency tests

--- webgui/tests/purchase.int.test.js
+++ webgui/tests/purchase.int.test.js
@@ -103,5 +103,32 @@ describe ("purchase API", function() {
     expect(status.text).toEqual(expect.stringContaining("Data Balance: 110.0 MB"));
     done();
   });
+  it('Post purchase api, stress consistency test', async (done) => {
+    // Generate many concurrent requests, and ensure the results match expected.
+    let promises = [];
+    for (i =0; i <100; ++i) {
+      promises.push(
+        test_request(app)
+        .post("/purchase/purchase")
+        .send({"package":"10000000"})
+        .set('X-Forwarded-For', '192.168.151.5')
+      );
+    }
+    results = await Promise.all(promises);
 
+    // Validate request results
+    for (i=0; i < 100; ++i){
+      expect(results[i].statusCode).toEqual(302);
+    }
+
+    // Validate Balance
+    const status = await test_request(app)
+      .get("/purchase")
+      .set('X-Forwarded-For', '192.168.151.5');
+    expect(status.statusCode).toEqual(200);
+    expect(status.text).toEqual(expect.stringContaining("Current Balance: $24750000"));
+    expect(status.text).toEqual(expect.stringContaining("Data Balance: 1.1 GB"));
+
+    done();
+  });
 });
