48de36552ff294806fb8e0001bf71702d060e553
Matt Johnson <matt9j@cs.washington.edu>
2021-02-26 19:33:23 -0800

    common-models: Remove extra startup logging

--- colte-common-models/index.js
+++ colte-common-models/index.js
@@ -4,8 +4,6 @@ const env = process.env.NODE_ENV || 'development';
 function buildCustomer() {
     let config = require("./knexfile.js")[env]
     config.connection.database = process.env.DB_NAME;
-    console.log("Building customer with config");
-    console.log(config);
     const knex = require('knex')(config);
     const customer = require('./customer.js');
 
