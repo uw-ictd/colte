81ac51d805f4aa1005f0a9bb8b9f1b564c20f7fc
Matt Johnson <matt9j@cs.washington.edu>
2021-07-28 16:42:36 -0700

    webadmin webgui common-models: Apply latest migrations on start
    When the server starts, ensure the database has the latest migrations
    applied. This is somewhat risky, since it means that migrations could
    be applied when a user does not explicitly request for them to be
    applied. In the future some kind of colte admin tool could handle
    migrations instead of automatically running them.

--- colte-common-models/index.js
+++ colte-common-models/index.js
@@ -10,7 +10,27 @@ function buildCustomer() {
   customer.register_knex(knex);
   return customer;
 }
+
+async function ensureLatestMigrations(migration_relative_path) {
+  let config = require("./knexfile.js")[env];
+  config.connection.database = process.env.DB_NAME;
+
+  // Normalize config migration directory to be an array
+  if (!Array.isArray(config.migrations.directory)) {
+    config.migrations.directory = [config.migrations.directory];
+  }
+
+  // Set the path for all entries
+  config.migrations.directory = config.migrations.directory.map(
+    (path_i) => migration_relative_path + path_i
+  );
+
+  const knex = require("knex")(config);
+  await knex.migrate.latest();
+}
+
 module.exports.buildCustomer = buildCustomer;
+module.exports.ensureLatestMigrations = ensureLatestMigrations;
 
 module.exports.getKnexInstance = require("knex");
 module.exports.knexFile = require("./knexfile.js");
