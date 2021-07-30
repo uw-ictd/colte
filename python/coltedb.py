3d878ad36730642e16d45dbe90858097cde6d93b
Matt Johnson <matt9j@cs.washington.edu>
2021-07-29 19:12:13 -0700

    python: Update coltedb for postgres
    This commit updates the coltedb script to connect to the new postgres
    database and use the up to date schema. The script currently has some
    edge cases and doesn't follow best practices for error handling, and
    could be greatly improved! This commit attempts to make the minimum
    change possible to work with postgres while ignoring these issues.

--- python/coltedb.py
+++ python/coltedb.py
@@ -3,7 +3,7 @@
 import ruamel.yaml
 import sys
 import os
-import mysql.connector
+import psycopg2
 import decimal
 import subprocess
 
@@ -35,7 +35,7 @@ if command == "help":
     exit(0)
 
 if os.geteuid() != 0:
-    print("haulagedb: Must run as root!")
+    print("coltedb: Must run as root!")
     exit(1)
 
 # load database connection information
@@ -47,15 +47,24 @@ with open(colte_vars, "r") as file:
 dbname = colte_data["mysql_db"]
 db_user = colte_data["mysql_user"]
 db_pass = colte_data["mysql_password"]
-db = mysql.connector.connect(host="localhost", user=db_user, passwd=db_pass, db=dbname)
+db = psycopg2.connect(host="localhost", user=db_user, password=db_pass, dbname=dbname)
 cursor = db.cursor()
 
 if command == "add":
-    if (len(sys.argv) > 8) or (len(sys.argv) < 7):
+    if (len(sys.argv) > 9) or (len(sys.argv) < 7):
         print(
-            'coltedb: incorrect number of args, format is "coltedb add imsi msisdn ip key opc [apn]". static IP and msisdn currently requred.'
+            'coltedb: incorrect number of args, format is "coltedb add imsi msisdn ip key opc [apn] [currency]". static IP and msisdn currently requred.'
         )
         open5gs_entry = "NULL"
+    elif len(sys.argv) == 9:
+        imsi = sys.argv[2]
+        msisdn = sys.argv[3]
+        ip = sys.argv[4]
+        ki = sys.argv[5]
+        opc = sys.argv[6]
+        apn = sys.argv[7]
+        currency = sys.argv[8]
+        open5gs_entry = imsi + " " + ip + " " + ki + " " + opc + " " + apn
     elif len(sys.argv) == 8:
         imsi = sys.argv[2]
         msisdn = sys.argv[3]
@@ -63,6 +72,7 @@ if command == "add":
         ki = sys.argv[5]
         opc = sys.argv[6]
         apn = sys.argv[7]
+        currency = "IDR"
         open5gs_entry = imsi + " " + ip + " " + ki + " " + opc + " " + apn
     else:
         imsi = sys.argv[2]
@@ -70,16 +80,60 @@ if command == "add":
         ip = sys.argv[4]
         ki = sys.argv[5]
         opc = sys.argv[6]
+        currency = "IDR"
         open5gs_entry = imsi + " " + ip + " " + ki + " " + opc
 
+    # TODO: error-handling? Check if imsi/msisdn/ip already in system?
+    print("coltedb: adding user " + str(imsi))
+    try:
+        cursor.execute("BEGIN TRANSACTION")
+
+        cursor.execute(
+            """
+            SELECT id FROM currencies WHERE code=%s
+            """,
+            [currency],
+        )
+        currency_ids = cursor.fetchall()
+        if len(currency_ids) != 1:
+            raise RuntimeError(
+                "Invalid currency code {}, try one like IDR or USD".format(currency)
+            )
+
+        cursor.execute(
+            """
+            INSERT INTO customers (imsi, username, balance, currency, enabled, admin, msisdn)
+            VALUES
+            (%s, %s, %s, %s, %s, %s, %s)
+            """,
+            [imsi, "", 0, currency_ids[0][0], True, False, msisdn],
+        )
+        cursor.execute("COMMIT")
+    except psycopg2.IntegrityError as e:
+        print("Failed to add user due to error {}".format(e))
+
     os.system("/etc/colte/colte_open5gsdb add " + open5gs_entry)
-    os.system("haulagedb add " + imsi + " " + msisdn + " " + ip)
+    os.system("haulagedb add " + imsi + " " + ip)
 
 elif command == "remove":
     if len(sys.argv) != 3:
         print('coltedb: incorrect number of args, format is "coltedb remove imsi"')
 
     imsi = sys.argv[2]
+
+    try:
+        cursor.execute("BEGIN TRANSACTION")
+
+        cursor.execute(
+            """
+            DELETE FROM customers WHERE imsi=%s
+            """,
+            [imsi],
+        )
+
+        cursor.execute("COMMIT")
+    except Exception as e:
+        print("Failed to remove due to error: {}".format(e))
 
     os.system("/etc/colte/colte_open5gsdb remove " + imsi)
     os.system("haulagedb remove " + imsi)
@@ -94,7 +148,7 @@ elif command == "topup":
     old_balance = 0
     new_balance = 0
 
-    commit_str = "SELECT balance FROM customers WHERE imsi = " + imsi + " FOR UPDATE"
+    commit_str = "SELECT balance FROM customers WHERE imsi = '" + imsi + "' FOR UPDATE"
     numrows = cursor.execute(commit_str)
     if numrows == 0:
         print("coltedb error: imsi " + str(imsi) + " does not exist!")
@@ -128,8 +182,9 @@ elif command == "topup":
             commit_str = (
                 "UPDATE customers SET balance = "
                 + str(new_balance)
-                + " WHERE imsi = "
+                + " WHERE imsi = '"
                 + imsi
+                + "'"
             )
             cursor.execute(commit_str)
             break
@@ -155,7 +210,7 @@ elif command == "admin":
     imsi = sys.argv[2]
 
     print("coltedb: giving admin privileges to user " + str(imsi))
-    commit_str = "UPDATE customers SET admin = 1 WHERE imsi = " + imsi
+    commit_str = "UPDATE customers SET admin = true WHERE imsi = '" + imsi + "'"
     cursor.execute(commit_str)
 
 elif command == "noadmin":
@@ -165,7 +220,7 @@ elif command == "noadmin":
     imsi = sys.argv[2]
 
     print("coltedb: removing admin privileges from user " + str(imsi))
-    commit_str = "UPDATE customers SET admin = 0 WHERE imsi = " + imsi
+    commit_str = "UPDATE customers SET admin = false WHERE imsi = '" + imsi + "'"
     cursor.execute(commit_str)
 
 else:
