bdf5998f9263426137ca4377c427bd00e23fa22e
Matt Johnson <matt9j@cs.washington.edu>
2021-12-06 19:19:02 -0800

    python: Run only installed colteconf modules
    We now support running colte with only a subset of the possible
    available components. This commit updates colteconf to run the
    configurations only for the installed components.

--- python/colteconf.py
+++ python/colteconf.py
@@ -4,8 +4,7 @@ import logging
 import ruamel.yaml
 import os
 
-import colteconf_cn_4g as colte_cn
-import colteconf_prepaid as colte_prepaid
+log = logging.getLogger(__name__)
 
 # This version saves comments/edits in YAML files
 yaml = ruamel.yaml.YAML()
@@ -14,25 +13,38 @@ yaml.indent(sequence=4, mapping=2, offset=2)
 # Input
 colte_vars = "/etc/colte/config.yml"
 
-log = logging.getLogger(__name__)
-
 if __name__ == "__main__":
     if os.geteuid() != 0:
         log.error("Must run as root!")
         raise PermissionError("The current implementation must run as root")
 
+    # Collect all installed subcomponents
+    config_modules = []
+    try:
+        import colteconf_cn_4g as colte_cn
+        config_modules.append(colte_cn)
+    except ModuleNotFoundError as e:
+        log.info("colte-cn-4g configuration not installed, skipping")
+
+    try:
+        import colteconf_prepaid as colte_prepaid
+        config_modules.append(colte_prepaid)
+    except ModuleNotFoundError as e:
+        log.info("colte-prepaid configuration not installed, skipping")
+
     # Read old vars and update yaml
     with open(colte_vars, "r") as file:
         colte_data = yaml.load(file.read())
 
-        colte_cn.update_all_components(colte_data)
-        colte_prepaid.update_all_components(colte_data)
+        for module in config_modules:
+            module.update_all_components(colte_data)
 
     # Restart everything to pick up new configurations, and don't restart
     # networkd while the EPC or metering are running.
-    colte_cn.stop_all_services()
-    colte_prepaid.stop_all_services()
+    for module in config_modules:
+        module.stop_all_services()
+
     os.system("systemctl restart systemd-networkd")
 
-    colte_cn.sync_service_state(colte_data)
-    colte_prepaid.sync_service_state(colte_data)
+    for module in config_modules:
+        module.sync_service_state(colte_data)
